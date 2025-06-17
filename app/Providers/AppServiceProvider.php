10b7d6721fa38ea97b0afef4f2413519ffe6b2b7
TheKiryuKha <Kirillzuyeu@gmail.com>
2025-06-17 17:36:59 +0300

    configure some defaults

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,23 +4,38 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use DB;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 final class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        $this->configureCommands();
+        $this->configureModels();
+        $this->configureUrls();
+    }
+
+    public function configureModels(): void
+    {
+        Model::unguard();
+        Model::shouldBeStrict();
+    }
+
+    public function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(app()->isProduction());
+    }
+
+    public function configureUrls(): void
+    {
+        URL::forceHttps(app()->isProduction());
     }
 }

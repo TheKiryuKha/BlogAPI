97f0468730d2760de603949e23b543b78730f205
TheKiryuKha <Kirillzuyeu@gmail.com>
2025-07-01 13:41:21 +0300

    test: post index and show

--- tests/Feature/Http/Api/V1/Post/ShowTest.php
+++ tests/Feature/Http/Api/V1/Post/ShowTest.php
@@ -2,11 +2,14 @@
 
 declare(strict_types=1);
 
+use App\Models\Post;
 use App\Models\User;
+
+beforeEach(fn () => $this->post = Post::factory()->create());
 
 it('returns the correct code if unauthentitficated', function () {
     $this->getJson(
-        route('api:v1:posts:index')
+        route('api:v1:posts:show', $this->post)
     )->assertStatus(
         401
     );
@@ -14,7 +17,7 @@ it('returns the correct code if unauthentitficated', function () {
 
 it('returns the correct code if authentitficated', function () {
     $this->actingAs(User::factory()->create())->getJson(
-        route('api:v1:posts:index')
+        route('api:v1:posts:show', $this->post)
     )->assertStatus(
         200
     );

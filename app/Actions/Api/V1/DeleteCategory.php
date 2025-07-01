39345560717a14821336f0b94d5cb41011f42c0d
TheKiryuKha <Kirillzuyeu@gmail.com>
2025-07-01 16:45:09 +0300

    refactor: return type of deleteCategory action

--- app/Actions/Api/V1/DeleteCategory.php
+++ app/Actions/Api/V1/DeleteCategory.php
@@ -10,13 +10,13 @@ use Illuminate\Database\Eloquent\Collection;
 
 final readonly class DeleteCategory
 {
-    public function handle(Category $category): Collection
+    public function handle(Category $category): bool
     {
         return DB::transaction(function () use ($category) {
             $category->posts()->update(['category_id' => 1]);
             $category->delete();
 
-            return $category->posts;
+            return true;
         });
     }
 }

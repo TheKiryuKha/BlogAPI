380d2a0d190812bbd8747f9617cf3a9b1ea9b4de
TheKiryuKha <Kirillzuyeu@gmail.com>
2025-07-01 14:22:55 +0300

    test: post store test with image

--- tests/Feature/Http/Api/V1/Post/StoreTest.php
+++ tests/Feature/Http/Api/V1/Post/StoreTest.php
@@ -2,9 +2,12 @@
 
 declare(strict_types=1);
 
+use App\Enums\PostStatus;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
+use Illuminate\Http\UploadedFile;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 test('user(admin) can create post', function () {
     $user = User::factory()->admin()->create();
@@ -58,4 +61,31 @@ test('user(reader) cannot create post', function () {
     $this->actingAs($user)
         ->post(route('api:v1:posts:store'), $post)
         ->assertStatus(403);
+});
+
+test("user(admin) can create post with an image", function(){
+    $user = User::factory()->admin()->create();
+    $category = Category::factory()->create();
+    $post = [
+        'title' => 'Test',
+        'user_id' => $user->id,
+        'category_id' => $category->id,
+        'status' => 'draft',
+        'content' => 'Test',
+        'image' => UploadedFile::fake()->image('test.png')
+    ];
+
+    $this->actingAs($user)
+        ->post(route('api:v1:posts:store'), $post)
+        ->assertStatus(201);
+
+    expect(Post::count())->toBe(1);
+
+    expect(Post::first())
+        ->title->toBe($post['title'])
+        ->user_id->toBe($post['user_id'])
+        ->category_id->toBe($post['category_id'])
+        ->status->toBe(PostStatus::Draft)
+        ->content->toBe($post['content'])
+        ->getMedia()->first()->toBeInstanceOf(Media::class);
 });
